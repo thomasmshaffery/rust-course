6301e308c9ac19f144908a0f8a56dfd79c4f82eb
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2026-02-11 00:00:50 -0500

    added cards vector

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -5,10 +5,17 @@ struct Deck {
 }
 
 fn main() {
-    let suits = vec!["Hearts", "Spades", "Diamonds", "Clubs"];
-    let values = vec!["Ace", "Two", "Three"];
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+    let values = ["Ace", "Two", "Three"];
 
+    let mut cards = vec![];
 
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
 
     let deck = Deck { cards: vec![] };
 
