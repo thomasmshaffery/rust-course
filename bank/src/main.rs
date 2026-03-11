be7bdb28f7b06c9f4d97591431de98c383865bc8
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2026-03-11 01:11:48 -0400

    added bank, account structs and impl

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account { id, holder, balance: 0 }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    let acc = Account::new(666, String::from("Tom"));
+    bank.accounts.add(acc);
+    println!("{:#?}", bank)
 }
